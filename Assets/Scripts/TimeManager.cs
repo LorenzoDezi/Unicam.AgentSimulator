0114b5638d268afb7b18a0e28af7c2fa3402c995
Lorenzo Dezi <lorenzo.dezi@studenti.unicam.it>
2017-12-07 16:52:12 +0100

    Prima commit

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    public static void ChangeTimeFactor(float timeFactor)
+    {
+        Time.timeScale = timeFactor;
+    }
+
+    public static void RestoreTimeFactor()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public static void StopTime()
+    {
+        Time.timeScale = 0f;
+    }
+
+}
